393fdf2e03a3b6349b21b96736a5e6ffcaecf427
biswajitd <33791038+biswajitd@users.noreply.github.com>
2026-09-01 12:36:58 +0530

    Remove Google Drive link and update button action

--- src/pages/SoftwareUtilities.tsx
+++ src/pages/SoftwareUtilities.tsx
@@ -2116,12 +2116,7 @@ const SoftwareUtilities = () => {
                   Download
                 </Button>
               </a>
-              <a
-                href="https://drive.google.com/file/d/1xH93Hr2cPIzpC9eAXQXU7GHlUsN8Espg/view?usp=drive_link"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex-1"
-              >
+             
               <a href="#macforge-features" className="flex-1">
                 <Button variant="outline" size="lg" className="w-full group">
                   <Info className="w-4 h-4" />
